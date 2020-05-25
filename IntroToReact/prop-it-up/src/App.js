c5d40e8d19c86322ea49f8c999c6476105790bd0
Cody Baermann <cbaermann@comcast.net>
2020-05-24 21:22:20 -0700

    need to fix incrementAge function. currently broken

--- IntroToReact/prop-it-up/src/App.js
+++ IntroToReact/prop-it-up/src/App.js
@@ -7,21 +7,55 @@ import MyNewComponet from './components/MyNewComponent';
 function App() {
   return (
     <div className="App">
-      <MyNewComponet name={<h1>Doe, Jane</h1>}/>
-      <MyNewComponet age={"Age: 45"}/>
+      <MyNewComponet>
+        {/* <button onClick={this.incrementAge}>Birthday Button</button> */}
+      </MyNewComponet>
+      {/* <MyNewComponet name={<h1>Doe, Jane</h1>}/>
+      <MyNewComponet age={45}/>
       <MyNewComponet hairColor={"Hair Color: Black"}/>
 
       <MyNewComponet name={<h1>Smith, John</h1>}/>
-      <MyNewComponet age={"Age: 88"}/>
-      <MyNewComponet hairColor={"Hair Color: Brown"}/>
+      {/* <MyNewComponet age={"Age: " + 88}/> */}
+      {/* <MyNewComponet hairColor={"Hair Color: Brown"}/>
 
       <MyNewComponet name={<h1>Fillmore, Millard</h1>}/>
-      <MyNewComponet age={"Age: 50"}/>
-      <MyNewComponet hairColor={"Hair Color: Brown"}/>
+      {/* <MyNewComponet age={"Age: " + 50}/> */}
+      {/* <MyNewComponet hairColor={"Hair Color: Brown"}/>
 
       <MyNewComponet name={<h1>Smith, Maria</h1>}/>
-      <MyNewComponet age={"Age: 62"}/>
-      <MyNewComponet hairColor={"Hair Color: Brown"}/>
+      {/* <MyNewComponet age={"Age: " + 62}/> */}
+      {/* <MyNewComponet hairColor={"Hair Color: Brown"}/> */} */} */} */}
+
+        {/* <MyNewComponet header={ "Doe, Jane" }>
+            <p>This is a child</p>
+            <p>Hair Color: Black</p>
+            <MyNewComponet age={`Age: ${44}`}/>
+            <button onClick={ () => alert("This button has been clicked")}>Birthday Button for Doe, Jane!</button>
+            {/* <button onClick={ () => { this.setState({ age: this.state.age + 1}) }}>Flip Switch</button> */}
+            {/* <button onClick={this.incrementAge}>Birthday Button</button> */}
+        {/* </MyNewComponet>  
+
+        <MyNewComponet header={ "Smith, John" }>
+            <p>This is a child</p>
+            <p>Hair Color: Brown</p>
+            <button onClick={ () => alert("This button has been clicked")}>Birthday button for Smith, John!</button>
+
+        </MyNewComponet>  
+
+        <MyNewComponet header={ "Fillmore, Millard" }>
+            <p>This is a child</p>
+            <p>Hair Color: Brown</p>
+            <button onClick={ () => alert("This button has been clicked")}>Birthday button for Fillmore, Millard!</button>
+
+        </MyNewComponet>  
+
+        <MyNewComponet header={ "Smith, Maria" }>
+            <p>This is a child</p>
+            <p>Hair Color: Brown</p>
+            <button onClick={ () => alert("This button has been clicked")}>Birthday button for Smith, Maria!</button>
+
+        </MyNewComponet>   */} */}
+
     </div>
   );
 }
--- IntroToReact/prop-it-up/src/components/MyNewComponent.js
+++ IntroToReact/prop-it-up/src/components/MyNewComponent.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 
 class MyNewComponet extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.incrementAge
+        }
+    }
+    incrementAge = () => {
+        this.setState({
+            age : this.state.age +=1
+        });
+    }
     render(){
         return(
             <div>
-                {this.props.name}
-                {this.props.age}
-                {this.props.hairColor}
+                <fieldset>
+                    <h1>Doe, Jane</h1>
+                    <p>Age: {this.state.age = 45}</p>
+                    <button onClick={this.incrementAge}>Birthday Button</button>
+                    
+                </fieldset>
+                <fieldset>
+                    <h1>Smith, John</h1>
+                    <p>Age: {this.state.age = 88}</p>
+                </fieldset>
             </div>
         );
     }
