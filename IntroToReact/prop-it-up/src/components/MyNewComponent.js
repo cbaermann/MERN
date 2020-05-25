ef9287cc2e8362bdcc58c73fd3035c28dce6d588
Cody Baermann <cbaermann@comcast.net>
2020-05-25 00:18:32 -0700

    fixed age incrementer

--- IntroToReact/prop-it-up/src/components/MyNewComponent.js
+++ IntroToReact/prop-it-up/src/components/MyNewComponent.js
@@ -4,26 +4,34 @@ class MyNewComponet extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            age: this.incrementAge
+            age: 45,
+            johnAge: 88
         }
     }
     incrementAge = () => {
         this.setState({
-            age : this.state.age +=1
+            age : this.state.age + 1
         });
+    }
+    incrementJohn = () => {
+        this.setState({
+            johnAge : this.state.johnAge +1
+        })
     }
     render(){
         return(
             <div>
                 <fieldset>
                     <h1>Doe, Jane</h1>
-                    <p>Age: {this.state.age = 45}</p>
+                    <p>Age: {this.state.age}</p>
                     <button onClick={this.incrementAge}>Birthday Button</button>
                     
                 </fieldset>
                 <fieldset>
                     <h1>Smith, John</h1>
-                    <p>Age: {this.state.age = 88}</p>
+                    <p>Age: {this.state.johnAge}</p>
+                    <button onClick={this.incrementJohn}>Birthday Button</button>
+
                 </fieldset>
             </div>
         );
