b8cd3596990caf3c5b98ab0961ef596d5269d948
Cody Baermann <cbaermann@comcast.net>
2020-05-28 15:21:39 -0700

    building react app using api

---
+++ APIs/use-effect/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import StarWars from './components/StarWars';
+
+function App() {
+  return (
+    <div className="App">
+      <StarWars/>
+    </div>
+  );
+}
+
+export default App;
