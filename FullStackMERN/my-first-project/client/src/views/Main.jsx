6e6863824556d470ccdb911f9584081f6d666868
Cody Baermann <cbaermann@comcast.net>
2020-06-01 14:24:37 -0700

    processes products through post form and renders json for now

--- FullStackMERN/my-first-project/client/src/views/Main.jsx
+++ FullStackMERN/my-first-project/client/src/views/Main.jsx
@@ -1,20 +1,12 @@
 import React, {useEffect, useState} from 'react';
 
 import axios from 'axios';
+import PersonForm from '../components/PersonForm';
 
 export default () => {
-    const [message, setMessage ] = useState("Loading...")
 
-    useEffect( () => {
-        axios.get('http://localhost:8000/api')
-            .then(response => {
-                setMessage(response.data.message)
-            })
-    }, [])
 return (
-    <div>
-        <h2>Message from backend: {message}</h2>
-    </div>
+    <PersonForm/>
 )
 
 }
