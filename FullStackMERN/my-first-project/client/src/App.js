91cd26b1996cd8768d9c12c222f5e51cf0d3e94d
Cody Baermann <cbaermann@comcast.net>
2020-06-02 09:22:11 -0700

    added getAllProducts and findOne

--- FullStackMERN/my-first-project/client/src/App.js
+++ FullStackMERN/my-first-project/client/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Router } from '@reach/router';
 import './App.css';
 
-import Main from './views/Main'
+import Main from './views/Main';
+import Detail from './views/Detail';
 
 function App() {
   return (
     <div className="App">
-      <Main />
+      <Router>
+        <Main path="/"/>
+        <Detail path="people/:id"/>
+      </Router>
     </div>
   );
 }
--- FullStackMERN/my-first-project/client/src/views/Main.jsx
+++ FullStackMERN/my-first-project/client/src/views/Main.jsx
@@ -2,11 +2,21 @@ import React, {useEffect, useState} from 'react';
 
 import axios from 'axios';
 import PersonForm from '../components/PersonForm';
+import PersonList from '../components/PersonList';
 
 export default () => {
+    const [people, setPeople] = useState([]);
 
-return (
-    <PersonForm/>
+    useEffect( ()=> {
+        axios.get('http://localhost:8000/api/people')
+            .then(res=>setPeople(res.data))
+            .catch(err=>console.log("Error: ", err))
+    })
+    return (
+        <>
+            <PersonForm/>
+            <PersonList people={people}/>
+        </>
 )
 
 }
